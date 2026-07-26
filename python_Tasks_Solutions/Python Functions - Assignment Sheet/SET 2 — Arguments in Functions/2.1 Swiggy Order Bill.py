8821298162d414f43fcbe5960d1830b8b96a4fbf
Lalisetti Venkat <venkat79814@gmail.com>
2026-07-26 15:54:26 +0530

    Add place_order function for Swiggy order billing
    Implemented place_order function to handle customer orders with variable items and charges.

--- python_Tasks_Solutions/Python Functions - Assignment Sheet/SET 2 — Arguments in Functions/2.1 Swiggy Order Bill.py
+++ python_Tasks_Solutions/Python Functions - Assignment Sheet/SET 2 — Arguments in Functions/2.1 Swiggy Order Bill.py
@@ -1,1 +1,21 @@
-
+#Write place_order(customer, *items, **charges).
+#place_order("Ravi", "Biryani", "Coke", "Gulab Jamun",delivery=40, gst=25, discount=50)
+# Output:
+# Customer : Ravi
+# Items ordered (3):
+#   1. Biryani
+#   2. Coke
+#   3. Gulab Jamun
+# Charges:
+#   delivery : 40
+#   gst      : 25
+#   discount : 50
+def place_order(customer,*items,**charges):
+    print('Customer :',customer)
+    print(f"Items Ordered({len(items)}):")
+    for i in range(len(items)):
+        print(f" {i+1}. {items[i]}")
+    print('Charges:')
+    for k,v in charges.items():
+        print(f" {k:<8} : {v}")
+place_order('Ravi','Biryani','Coke','Gulab Jamun',delivery=40,gst=25,discount=50)

ab4a2db9b1c989e44ef3b275ac126aa7fe58f895
Lalisetti Venkat <venkat79814@gmail.com>
2026-07-26 16:37:29 +0530

    Update 2.4 The Shopping Cart Bug.py

--- python_Tasks_Solutions/Python Functions - Assignment Sheet/SET 2 — Arguments in Functions/2.4 The Shopping Cart Bug.py
+++ python_Tasks_Solutions/Python Functions - Assignment Sheet/SET 2 — Arguments in Functions/2.4 The Shopping Cart Bug.py
@@ -1,1 +1,14 @@
-
+# def add_to_cart(item, cart=[]):
+#     cart.append(item)
+#     return cart
+# print(add_to_cart("pen"))
+# print(add_to_cart("book"))
+# print(add_to_cart("bag"))
+# Buggy output:
+# ['pen']
+# ['pen', 'book']
+# ['pen', 'book', 'bag']
+# Expected output after your fix:
+# ['pen']
+# ['book']
+# ['bag']
